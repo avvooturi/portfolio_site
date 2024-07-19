9251c45aa19ddbd9ad00b758dc0598634e5f31ad
avvooturi <114264497+avvooturi@users.noreply.github.com>
2024-07-19 19:25:51 -0400

    about me and border made

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,7 +5,7 @@ import { ReactTyped } from "react-typed";
 function Logo() {
   return (
     <>
-    <div className='flex space-x-2'>
+    <div className='absolute inset-x-20 flex space-x-2'>
       <img
         className="rounded-lg ml-2 float-left"
         src={logo}
@@ -23,11 +23,11 @@ function Logo() {
 function Title() {
   return (
     <>
-      <div className='absolute inset-y-64 left-0'>
+      <div className='absolute inset-y-64 left-20'>
         <h1 className='text-white text-7xl'>
-          Welcome to {" "}
+          Hello, I'm {" "}
           <ReactTyped
-            strings={["Another React App"]}
+            strings={["Arjun Vooturi", "A Software Engineer"]}
             typeSpeed={100}
             loop
             backSpeed={20}
@@ -40,12 +40,29 @@ function Title() {
   );
 }
 
+function About() {
+  return (
+    <>
+    <div className='absolute inset-x-32 top-3/4 flex justify-center items-center'>
+      <h1 className='text-4xl text-white'>
+      About Me
+      </h1>
+      <h3 className='text-2xl text-white ml-20'>
+      Let's try this
+      </h3>
+    </div>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <>
     <div className='min-h-screen bg-black'>
       <Logo/>
       <Title/>
+      <div className='absolute inset-x-0 top-2/3 border-t-4 border-indigo-500 border-8'></div>
+      <About/>
     </div>
     </>
   );
