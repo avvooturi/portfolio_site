26f81af8814bcf8ce6231a922650198022580a88
avvooturi <114264497+avvooturi@users.noreply.github.com>
2024-07-18 21:38:11 -0400

    logo and main title finished

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,52 @@
 import logo from './da_bear.png';
 import './App.css';
+import { ReactTyped } from "react-typed";
 
-function FirstRow() {
+function Logo() {
   return (
     <>
-    <img
-      className="bear"
-      src={logo}
-      alt="bear"
-      width={100}
-      height={100} />
-      <h1 className='text-3xl font-bold underling'>
-        Current Title
-      </h1>
+    <div className='flex space-x-2'>
+      <img
+        className="rounded-lg ml-2 float-left"
+        src={logo}
+        alt="bear"
+        width={30}
+        height={30} />
+        <h6 className='font-bold underling text-white'>
+          avvooturi
+        </h6>
+    </div>
       </>
+  );
+}
+
+function Title() {
+  return (
+    <>
+      <div className='absolute inset-y-64 left-0'>
+        <h1 className='text-white text-7xl'>
+          Welcome to {" "}
+          <ReactTyped
+            strings={["Another React App"]}
+            typeSpeed={100}
+            loop
+            backSpeed={20}
+            cursorChar='>'
+            showCursor={true}
+          />
+        </h1>
+      </div>
+    </>
   );
 }
 
 export default function App() {
   return (
-    <FirstRow/>
+    <>
+    <div className='min-h-screen bg-black'>
+      <Logo/>
+      <Title/>
+    </div>
+    </>
   );
 }
