3190f41bb9b1e4bbbe8b2c74621020609d79d280
avvooturi <114264497+avvooturi@users.noreply.github.com>
2024-07-20 17:53:39 -0400

    made title into whole page, will format about me inna second

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,6 @@ import { ReactTyped } from "react-typed";
 
 function Logo() {
   return (
-    <>
     <div className='absolute inset-x-20 flex space-x-2'>
       <img
         className="rounded-lg ml-2 float-left"
@@ -12,41 +11,37 @@ function Logo() {
         alt="bear"
         width={30}
         height={30} />
-        <h6 className='font-bold underling text-white'>
-          avvooturi
-        </h6>
+      <h6 className='font-bold underline text-white'>
+        avvooturi
+      </h6>
     </div>
-      </>
   );
 }
 
 function Title() {
   return (
-    <>
-      <div className='absolute inset-y-64 left-20'>
-        <h1 className='text-white text-7xl'>
-          Hello, I'm {" "}
-          <ReactTyped
-            strings={["Arjun Vooturi", "A Software Engineer"]}
-            typeSpeed={100}
-            loop
-            backSpeed={20}
-            cursorChar='>'
-            showCursor={true}
-          />
-        </h1>
-      </div>
-    </>
+    <div className='min-h-screen flex items-center justify-center'>
+      <h1 className='text-white text-7xl'>
+        Hello, I'm {" "}
+        <ReactTyped
+          strings={["Arjun Vooturi", "A Software Engineer"]}
+          typeSpeed={100}
+          loop
+          backSpeed={20}
+          cursorChar='>'
+          showCursor={true}
+        />
+      </h1>
+    </div>
   );
 }
 
 function About() {
   return (
-    <>
-    <div className='absolute inset-x-32 top-3/4 flex justify-center items-center'>
+    <div className='flex justify-center items-center pt-20'>
       <div>
         <h1 className='text-4xl text-white'>
-        About Me
+          About Me
         </h1>
         <h3 className='text-xl text-white mt-4 break-normal'>
           When we pull up yeah on the toad, I keep clips with me so what do you kno? I had already knew they finna hate spanish language last week she nagged me this week I got new goals
@@ -60,20 +55,32 @@ function About() {
         height={225}
       />
     </div>
-    </>
+  );
+}
+
+function Skills() {
+  return (
+    <div className='flex justify-center items-center pt-20'>
+      <div>
+        <h1 className='text-4xl text-white'>
+          Float
+        </h1>
+        <h3 className='text-xl text-white mt-4 break-normal'>
+          I say everybody yell FreeRIQ
+        </h3>
+      </div>
+    </div>
   );
 }
 
 export default function App() {
   return (
-    <>
-    <div className='min-h-screen bg-black'>
-      <Logo/>
-      <Title/>
-      <div className='absolute inset-x-0 top-2/3 border-t-4 border-indigo-500 border-8'></div>
-      <About/>
-      <About/>
+    <div className='bg-black'>
+      <Logo />
+      <Title />
+      <div className='border-t-4 border-indigo-500 border-8'></div>
+      <About />
+      <Skills />
     </div>
-    </>
   );
 }
